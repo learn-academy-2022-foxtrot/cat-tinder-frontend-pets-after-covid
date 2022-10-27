5fa0f75cba18d32cf93bb2ee278594c3eca9d7e2
aribrashear <aribrashear@gmail.com>
2022-10-27 16:58:23 -0700

    Updated styling for App / Home.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,51 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const Home = (params) => {
   return (
     <>
       <div className="hero-container">
-        <div className="hero-text">
-          <h1 className="header-primary">Welcome to the #PAC.</h1>
+        <div className="hero-flex">
+          <div className="hero-text">
+            <h1 className="header-primary">
+              Welcome to the <span>#PAC.</span>
+            </h1>
+            <p>
+              The devastating lockdowns of 2020 were unprecidented; not only for
+              the millions of lives lost worldwide to the COVID-19 virus, but
+              also for the mental anguish that was associated with extreme
+              prolonged isolation. Humans crave touch. We need to feel a sense
+              of companionship and love and be able to reciprocate those
+              feelings. It was during this period where millions of us turned to
+              animals as a way to fill that void. The results speak for
+              themselves.
+            </p>
+          </div>
+          <div className="hero-buttons">
+            <Button
+              sx={{
+                color: "#fff",
+                borderColor: "#fff",
+                fontSize: "1.6rem",
+                textShadow: "1px 1px #000",
+                minWidth: "17rem",
+                backgroundColor: "rgba(0, 0, 0, 0.3)",
+              }}
+              variant="outlined"
+            >
+              Learn More <KeyboardArrowDownIcon />
+            </Button>
+            <Button
+              sx={{ minWidth: "17rem", fontSize: "1.6rem" }}
+              variant="contained"
+            >
+              Sign Up
+            </Button>
+          </div>
         </div>
       </div>
       <p>
